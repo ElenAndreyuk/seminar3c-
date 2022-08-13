025f40d8613277a0bcde1da3807e4191bde24f73
Elena <femtochka@yahoo.com>
2022-08-13 16:35:24 +0300

    третья задача

---
+++ task003/Program.cs
@@ -0,0 +1,27 @@
+﻿// выводим кубы чисел 
+Console.WriteLine("Введите целое число");
+int n = int.Parse(Console.ReadLine());
+if (n > 0)
+{
+    for (int i = 1; i <= n ; i++)
+    {
+        Console.Write(Math.Pow(i, 3));
+
+        if(i < n)
+        {
+            Console.Write(", ");
+
+        }
+    }
+}
+else
+{
+    for (int i = -1; i >= n ; i = i -1)
+    {
+     Console.Write(Math.Pow(i, 3));
+        if(i > n)
+        {
+            Console.Write(", ");
+        }
+    }
+}
